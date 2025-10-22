ac0b7974a1dd43499632d83ce317d6399c56991b
sebastianmyrback <sebastian.myrback@hotmail.com>
2025-10-22 11:37:57 +0200

    ChatGPT cleanup and fixed crucial bug in boundary.cpp leading to different results in runtime

--- cpp/problem/boundary.cpp
+++ cpp/problem/boundary.cpp
@@ -101,66 +101,64 @@ BoundaryDirichlet<Mesh2>::BoundaryDirichlet(const cutspace_t &Vh, const Barycent
                 int jfac = ifac;
                 int kn_micro = active_mesh.ElementAdj(k_micro, jfac);
 
-            // a neighboring element can be 1) inside of the domain, 2) another cut element, 
-            // 3) outside of the domain but in the same macro element, or 4) inside of the domain and in another macro element
-                if ((kn_micro != -1) ||(active_mesh.inverse_active_macro_map[k_micro] == active_mesh.inverse_active_macro_map[kn_micro]))
+                if (kn_micro != -1) 
                     continue;
 
-                    // const int kn_macro = active_mesh.inverse_active_macro_map.at(kn_micro);
-                    // std::cout << "kn_micro = " << kn_micro << "\n";
-                    
-                    // assert(!active_mesh.is_macro_cut(kn_macro) || !active_mesh.is_macro_interior(kn_macro));
-
-                    const int kb = active_mesh.idxElementInBackMesh(k_micro);
-                    const auto &FK(Vh[k_micro]);  // Get the finite element for the current element in the finite element space
-                    const auto &T(Th_background[kb]);
-                    const int domain = FK.get_domain();
-
-                    std::vector<Rd> dof_points(FK.tfe->NbPtforInterpolation);
-                    FK.tfe->global_dofs(T, dof_points);
-
-                    int ndof_edge_per_component = FK.tfe->ndfonEdge / Vh.N;
-
-                    // std::cout << "Macro element " << km << "\n";
+                // const int kn_macro = active_mesh.inverse_active_macro_map.at(kn_micro);
+                // std::cout << "kn_micro = " << kn_micro << "\n";
                 
-                    for (int ic = 0; ic < Vh.N; ++ic) {
-                        for (size_t df_loc = 0, df = FK.dfcbegin(ic); df < FK.dfcend(ic); ++df, ++df_loc) {
+                // assert(!active_mesh.is_macro_cut(kn_macro) || !active_mesh.is_macro_interior(kn_macro));
 
-                            // here need a fct that give the item from the dof for P3 for example
-                            int id_item       = df_loc;
-                            bool is_on_border = false;
+                const int kb = active_mesh.idxElementInBackMesh(k_micro);
+                const auto &FK(Vh[k_micro]);  // Get the finite element for the current element in the finite element space
+                const auto &T(Th_background[kb]);
+                const int domain = FK.get_domain();
 
-                            // case 1: dof is on node. E.g. (0, 1, 2) if triangle
-                            if (id_item < T.nv) {
-                                for (int i = 0; i < elt_t::nva; ++i) {
+                std::vector<Rd> dof_points(FK.tfe->NbPtforInterpolation);
+                FK.tfe->global_dofs(T, dof_points);
 
-                                    int i_e = elt_t::nvedge.at(ifac).at(i);
-                                    if (i_e == id_item) {
-                                        is_on_border = true;
-                                        break;
-                                    }
-                                }
-                            }
-                            // case 2: dof is on an edge (or face)
-                            else if (id_item < T.nv + T.ne * ndof_edge_per_component) { // df if on an edge
+                int ndof_edge_per_component = FK.tfe->ndfonEdge / Vh.N;
 
-                                int id_face = (id_item - T.nv) / ndof_edge_per_component;
+                // std::cout << "Macro element " << km << "\n";
+            
+                for (int ic = 0; ic < Vh.N; ++ic) {
+                    for (size_t df_loc = 0, df = FK.dfcbegin(ic); df < FK.dfcend(ic); ++df, ++df_loc) {
 
-                                if (id_face == ifac) {
+                        // here need a fct that give the item from the dof for P3 for example
+                        int id_item       = df_loc;
+                        bool is_on_border = false;
+
+                        // case 1: dof is on node. E.g. (0, 1, 2) if triangle
+                        if (id_item < T.nv) {
+                            for (int i = 0; i < elt_t::nva; ++i) {
+
+                                int i_e = elt_t::nvedge.at(ifac).at(i);
+                                if (i_e == id_item) {
                                     is_on_border = true;
+                                    break;
                                 }
-                            }
-                            if (is_on_border) {
-
-                                Rd P                   = dof_points.at(df_loc);
-                                // std::cout << "Boundary point P = " << P << "\n";
-                                size_t df_glob         = FK.loc2glb(df);
-                                boundary_dofs[df_glob] = DofData<D>(kb, domain, ic, P);
                             }
                         }
+                        // case 2: dof is on an edge (or face)
+                        else if (id_item < T.nv + T.ne * ndof_edge_per_component) { // df if on an edge
+
+                            int id_face = (id_item - T.nv) / ndof_edge_per_component;
+
+                            if (id_face == ifac) {
+                                is_on_border = true;
+                            }
+                        }
+                        if (is_on_border) {
+
+                            Rd P                   = dof_points.at(df_loc);
+                            // std::cout << "Boundary point P = " << P << "\n";
+                            size_t df_glob         = FK.loc2glb(df);
+                            boundary_dofs[df_glob] = DofData<D>(kb, domain, ic, P);
+                        }
                     }
-                }                
-            }
+                }
+            }                
+        }
     
         // std::cout << std::endl;
     }
